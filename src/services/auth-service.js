bf26c4442d1775eddca634702d38fc28c766eba1
unknown <tang.kevi@northeastern.edu>
2022-03-23 18:42:53 -0400

    Profile/Logout

--- src/services/auth-service.js
+++ src/services/auth-service.js
@@ -10,3 +10,11 @@ const api = axios.create({
 export const signup = (user) =>
     api.post(`${AUTH_API}/signup`, user)
     .then(response => response.data);
+
+export const profile = () =>
+    api.post(`${AUTH_API}/profile`)
+    .then(response => response.data);
+
+export const logout = (user) =>
+    api.post(`${AUTH_API}/logout`, user)
+    .then(response => response.data);
