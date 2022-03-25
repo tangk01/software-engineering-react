8edb6354bfc8dc5409a6136cf7b480c5fc8def04
unknown <tang.kevi@northeastern.edu>
2022-03-25 17:58:40 -0400

    Signup

--- src/components/profile/login.js
+++ src/components/profile/login.js
@@ -1,6 +1,7 @@
+import React from "react";
 import {useNavigate} from "react-router-dom";
 import {useState} from "react";
-import * as service from "../../services/auth-service";
+import * as service from "../../services/security-service";
 
 export const Login = () => {
   const [loginUser, setLoginUser] = useState({});
@@ -12,14 +13,17 @@ export const Login = () => {
   return (
       <div>
         <h1>Login</h1>
-        <input onChange={(e) =>
-            setLoginUser({...loginUser,
-              username: e.target.value})}/>
-        <input onChange={(e) =>
-            setLoginUser({...loginUser,
-              password: e.target.value})}/>
-        <button onClick={login}>
-          Login</button>
+        <input className="mb-2 form-control"
+               onChange={(e) =>
+                   setLoginUser({...loginUser, username: e.target.value})}
+               placeholder="username"/>
+        <input className="mb-2 form-control"
+               onChange={(e) =>
+                   setLoginUser({...loginUser, password: e.target.value})}
+               placeholder="password" type="password"/>
+        <button onClick={login}
+                className="btn btn-primary mb-5">Login
+        </button>
       </div>
   );
 };
