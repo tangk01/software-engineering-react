595650841a2bec66539889f3387003f893ae1b83
unknown <tang.kevi@northeastern.edu>
2022-03-26 17:32:42 -0400

    URL

--- src/services/auth-service.js
+++ src/services/auth-service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 // const BASE_URL = "https://cs5500-01-sp22.herokuapp.com/api";
 // const BASE_URL = "http://localhost:4000/api";
-const BASE_URL = "https://cs5500node.herokuapp.com/"
+const BASE_URL = "https://cs5500node.herokuapp.com"
     // process.env.REACT_APP_BASE_URL
 
 const AUTH_API = `${BASE_URL}/api/auth`
