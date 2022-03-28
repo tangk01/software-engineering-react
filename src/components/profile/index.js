0cf6f527c8bc84304ca57080dc8eff54ca36ecf8
unknown <tang.kevi@northeastern.edu>
2022-03-28 17:03:42 -0400

    Network

--- src/components/profile/index.js
+++ src/components/profile/index.js
@@ -1,26 +1,23 @@
-import React, {useEffect, useState} from "react";
-import {HashRouter, Link, Route, Routes, useNavigate, useLocation} from "react-router-dom";
-import * as service from "../../services/auth-service"
+import * as service from "./auth-service"
+import {useNavigate} from "react-router-dom";
+import {useEffect, useState} from "react";
 
 const Profile = () => {
   const navigate = useNavigate();
   const [profile, setProfile] = useState({});
-
   useEffect(async () => {
     try {
       const user = await service.profile();
       setProfile(user);
-      navigate('/profile/mytuits');
+      // navigate('/profile/mytuits');
     } catch (e) {
       navigate('/login');
     }
   }, []);
-
   const logout = () => {
     service.logout()
     .then(() => navigate('/login'));
   }
-
   return(
       <div>
         <h4>{profile.username}</h4>
