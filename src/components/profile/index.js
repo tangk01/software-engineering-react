a89e777613e5c0cce3a5e2c732e023f2d0bb54b1
unknown <tang.kevi@northeastern.edu>
2022-03-27 22:13:31 -0400

    Network

--- src/components/profile/index.js
+++ src/components/profile/index.js
@@ -5,18 +5,22 @@ import * as service from "../../services/auth-service"
 const Profile = () => {
   const navigate = useNavigate();
   const [profile, setProfile] = useState({});
+
   useEffect(async () => {
     try {
       const user = await service.profile();
       setProfile(user);
+      navigate('/profile/mytuits');
     } catch (e) {
       navigate('/login');
     }
   }, []);
+
   const logout = () => {
     service.logout()
     .then(() => navigate('/login'));
   }
+
   return(
       <div>
         <h4>{profile.username}</h4>
