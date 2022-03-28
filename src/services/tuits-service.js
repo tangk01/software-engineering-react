a1a431cfc88c0e02608a6ee711b5cc3f3e40c0d9
unknown <tang.kevi@northeastern.edu>
2022-03-28 18:21:31 -0400

    Tuit Design

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,31 +1,37 @@
 import axios from "axios";
-const TUITS_API = "https://cs5500node.herokuapp.com/tuits";
-const USERS_API = "https://cs5500node.herokuapp.com/users";
+const BASE_URL = process.env.REACT_APP_BASE_URL;
+
+const TUITS_API = `${BASE_URL}/tuits`;
+const USERS_API = `${BASE_URL}/users`;
+
+const api = axios.create({
+        withCredentials: true
+});
 
 export const findAllTuits = () =>
-    axios.get(TUITS_API)
+    api.get(TUITS_API)
         .then(response => response.data);
 
 export const findTuitById = (tid) =>
-    axios.get(`${TUITS_API}/${tid}`)
+    api.get(`${TUITS_API}/${tid}`)
         .then(response => response.data);
 
-export const findTuitByUser = (uid) =>
-    axios.get(`${USERS_API}/${uid}/tuits`)
+export const findTuitsByUser = (uid) =>
+    api.get(`${USERS_API}/${uid}/tuits`)
         .then(response => response.data);
 
 export const createTuit = (uid, tuit) =>
-    axios.post(`${USERS_API}/${uid}/tuits`, tuit)
+    api.post(`${USERS_API}/${uid}/tuits`, tuit)
         .then(response => response.data);
 
 export const updateTuit = (tid, tuit) =>
-    axios.post(`${TUITS_API}/${tid}`, tuit)
+    api.post(`${TUITS_API}/${tid}`, tuit)
         .then(response => response.data);
 
 export const deleteTuit = (tid) =>
-    axios.delete(`${TUITS_API}/${tid}`)
+    api.delete(`${TUITS_API}/${tid}`)
         .then(response => response.data);
 
 export const deleteTuitsByTuit = (tuit) =>
-    axios.get(`${TUITS_API}/${tuit}/delete`)
+    api.get(`${TUITS_API}/${tuit}/delete`)
         .then(response => response.data);
