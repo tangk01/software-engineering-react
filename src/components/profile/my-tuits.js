193a37921e605198f6b41acda2d60615b19e8fed
unknown <tang.kevi@northeastern.edu>
2022-03-28 23:18:18 -0400

    Network Error

--- src/components/profile/my-tuits.js
+++ src/components/profile/my-tuits.js
@@ -5,12 +5,15 @@ import Tuits from "../tuits";
 const MyTuits = () => {
   const [tuits, setTuits] = useState([]);
   const findMyTuits = () =>
-      service.findTuitsByUser("my")
+      service.findTuitsByUser("me")
       .then(tuits => setTuits(tuits));
   useEffect(findMyTuits, []);
+  const deleteTuit = (tid) =>
+      service.deleteTuit(tid)
+      .then(findMyTuits);
   return(
       <Tuits tuits={tuits}
-             refreshTuits={findMyTuits}/>
+             deleteTuit={deleteTuit}/>
   );
 };
 
