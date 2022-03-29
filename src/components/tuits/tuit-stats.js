5f01103b124ce9511a080333a3930e3a09e01fb0
unknown <tang.kevi@northeastern.edu>
2022-03-29 19:25:49 -0400

    Likes

--- src/components/tuits/tuit-stats.js
+++ src/components/tuits/tuit-stats.js
@@ -4,22 +4,22 @@ const TuitStats = ({tuit, likeTuit = () => {}}) => {
   return (
       <div className="row mt-2">
         <div className="col">
-          <i className="far fa-message me-1"></i>
+          <i className="fa-solid fa-thumbs-up"></i>
           {tuit.stats && tuit.stats.replies}
         </div>
         <div className="col">
-          <i className="far fa-retweet me-1"></i>
+          <i className="fa-solid fa-thumbs-up"></i>
           {tuit.stats && tuit.stats.retuits}
         </div>
         <div className="col">
           <span onClick={() => likeTuit(tuit)}>
               {
                 tuit.stats && tuit.stats.likes && tuit.stats.likes > 0 &&
-                <i className="fas fa-heart me-1" style={{color: 'red'}}></i>
+                <i className="fa-solid fa-thumbs-up" style = {{color: 'black'}}></i>
               }
             {
               tuit.stats && tuit.stats.likes && tuit.stats.likes <= 0 &&
-              <i className="far fa-heart me-1"></i>
+              <i className="fa-solid fa-thumbs-up"></i>
             }
             {tuit.stats && tuit.stats.likes}
           </span>
