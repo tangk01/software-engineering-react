dc6bd96dea0af841a0db9967f8b136b6f785d82f
unknown <tang.kevi@northeastern.edu>
2022-03-28 23:46:23 -0400

    Network Error

--- src/components/profile/index.js
+++ src/components/profile/index.js
@@ -25,17 +25,7 @@ const Profile = () => {
         <h6>@{profile.username}</h6>
         <button onClick={logout}>
           Logout</button>
-
-{/*        <Routes>
-          <Route path="/mytuits"
-                 element={<MyTuits/>}/>
-          <Route path="/tuits-and-replies"
-                 element={<TuitsAndReplies/>}/>
-          <Route path="/media"
-                 element={<Media/>}/>
-          <Route path="/mylikes"
-                 element={<MyLikes/>}/>
-        </Routes>*/}
+        
       </div>
   );
 };
