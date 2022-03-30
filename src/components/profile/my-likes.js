b786ee6a2354c756b5835b5c6a0d78e5864d5113
unknown <tang.kevi@northeastern.edu>
2022-03-29 22:28:17 -0400

    Implementation of AllTuitsLikedByUser and Likes Screen

--- src/components/profile/my-likes.js
+++ src/components/profile/my-likes.js
@@ -1,7 +1,18 @@
+import Tuits from "../tuits";
+import * as service from "../../services/likes-service";
+import {useEffect, useState} from "react";
+
 const MyLikes = () => {
+  const [likedTuits, setLikedTuis] = useState([]);
+  const findTuitsILike = () =>
+      service.findAllTuitsLikedByUser("me")
+      .then((tuits) => setLikedTuis(tuits));
+  useEffect(findTuitsILike, []);
+
   return(
       <div>
-        <h1>Likes</h1>
+        <Tuits tuits={likedTuits}
+               refreshTuits={findTuitsILike}/>
       </div>
   );
 };
--- src/services/likes-service.js
+++ src/services/likes-service.js
@@ -11,6 +11,12 @@ export const userTogglesTuitLikes = (uid, tid) =>
     api.put(`${USERS_API}/${uid}/likes/${tid}`)
     .then(response => response.data);
 
+export const findAllTuitsLikedByUser = (userId) =>
+    api.get(`${USERS_API}/${userId}/likes`)
+    .then(response => response.data);
+
+
+
 export const userTogglesTuitDislikes = (uid, tid) =>
     api.put(`${USERS_API}/${uid}/dislikes/${tid}`)
     .then(response => response.data);
