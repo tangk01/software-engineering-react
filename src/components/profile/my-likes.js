e632a68d15cb44521e47949ccd6d075c4f8454dc
unknown <tang.kevi@northeastern.edu>
2022-03-29 22:53:11 -0400

    Minor Bug Fixes

--- src/components/profile/my-likes.js
+++ src/components/profile/my-likes.js
@@ -5,7 +5,7 @@ import {useEffect, useState} from "react";
 const MyLikes = () => {
   const [likedTuits, setLikedTuis] = useState([]);
   const findTuitsILike = () =>
-      service.findAllTuitsLikedByUser("me")
+      service.findAllTuitsLikedByUser("my")
       .then((tuits) => setLikedTuis(tuits));
   useEffect(findTuitsILike, []);
 
