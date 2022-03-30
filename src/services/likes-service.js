3342b51c7591a7bd3106c02dc9cc28375bc8c517
unknown <tang.kevi@northeastern.edu>
2022-03-29 21:03:49 -0400

    Dislike UI

--- src/services/likes-service.js
+++ src/services/likes-service.js
@@ -10,3 +10,7 @@ const api = axios.create({
 export const userTogglesTuitLikes = (uid, tid) =>
     api.put(`${USERS_API}/${uid}/likes/${tid}`)
     .then(response => response.data);
+
+export const userTogglesTuitDislikes = (uid, tid) =>
+    api.put(`${USERS_API}/${uid}/dislikes/${tid}`)
+    .then(response => response.data);
