5f50b26477bb06f1657506bc7513663451ac2e5a
unknown <tang.kevi@northeastern.edu>
2022-03-30 00:16:41 -0400

    Likes-service Testing

--- src/tests/likes-service.test.js
+++ src/tests/likes-service.test.js
@@ -155,51 +155,6 @@ describe('toggle dislikes on tuit', () => {
   });
 });
 
-describe('toggle likes on tuit', () => {
-  // Mock user for testing
-  const mockUser = {
-    username: 'mockUser',
-    password: 'test123',
-    email: 'mockUser@email.com'
-  };
-
-  // Test Tuit
-  const message = {
-    tuit: "Create Test Tuit"
-  };
-
-  // Setup test before running test
-  beforeAll(() => {
-    // remove mockuser and tuit
-    deleteUsersByUsername(mockUser.username);
-    return deleteTuitsByTuit(message.tuit);
-  })
-
-  // Clean up after test runs
-  afterAll(() => {
-    // remove any data we created
-    deleteUsersByUsername(mockUser.username);
-    return deleteTuitsByTuit(message.tuit);
-  })
-
-  test('toggle likes on a tuit', async () => {
-    //Insert new tuit
-    const user = await createUser(mockUser);
-    const tuit = await createTuit(user._id, message);
-
-    //Check to see if the count of likes changes
-    expect(tuit.stats.likes).toEqual(0);
-
-    await userTogglesTuitLikes(user._id, tuit._id);
-
-    expect(tuit.stats.likes).toEqual(1);
-
-    await userTogglesTuitLikes(user._id, tuit._id);
-
-    expect(tuit.stats.likes).toEqual(0);
-  });
-});
-
 describe('find all tuits disliked by user', () => {
   // Mock user for testing
   const mockUser = {
